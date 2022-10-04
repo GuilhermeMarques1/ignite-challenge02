6608c103e519df59e5e344a66998927e99cfce82
Guilherme Marques ALEST <guilherme.camara@alest.com.br>
2022-10-04 12:17:42 -0300

    feat: Component of SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+import { Button } from './Button';
+
+import { GenreResponseProps } from '../App';
+
+interface SideBarProps {
+  genres: Array<GenreResponseProps>,
+  selectedGenreId: number,
+  setSelectedGenreId: any,
+}
+
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => setSelectedGenreId(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
